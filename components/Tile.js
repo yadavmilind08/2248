b2f3677bca2dbee3a5e1c8b70a0aee5d08959b0b
Milind Yadav <yadavmilind08@gmail.com>
2022-12-25 14:54:34 +0530

    Refactored style

--- components/Tile.js
+++ components/Tile.js
@@ -1,9 +1,10 @@
 import { StyleSheet, View, Text } from "react-native";
+import { colors } from "../util/board";
 
 export const Tile = ({ value, onCellLayout }) => {
   return (
     <View
-      style={[styles.cellStyle, styles[`cell${value}`]]}
+      style={[styles.cellStyle, { backgroundColor: colors[value] }]}
       onLayout={onCellLayout}
     >
       <Text style={styles.textStyle}>{value}</Text>
@@ -19,39 +20,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     margin: 20,
     borderRadius: 10,
-  },
-  cell2: {
-    backgroundColor: "#ffa500",
-  },
-  cell4: {
-    backgroundColor: "#27cfcf",
-  },
-  cell8: {
-    backgroundColor: "#ffc0cb",
-  },
-  cell16: {
-    backgroundColor: "#ffd700",
-  },
-  cell32: {
-    backgroundColor: "#adff2f",
-  },
-  cell64: {
-    backgroundColor: "#e9c46a",
-  },
-  cell128: {
-    backgroundColor: "#daa520",
-  },
-  cell256: {
-    backgroundColor: "#663399",
-  },
-  cell512: {
-    backgroundColor: "#00ffff",
-  },
-  cell1024: {
-    backgroundColor: "#6a5ab0",
-  },
-  cell2048: {
-    backgroundColor: "#7fffd4",
   },
   textStyle: {
     fontSize: 30,
